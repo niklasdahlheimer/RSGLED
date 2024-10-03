9acf5f6d3f9e345f4b5a96c573bc0b2a4d3517ba
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2024-10-03 14:44:07 +0200

    format

--- include/ledController.h
+++ include/ledController.h
@@ -6,42 +6,42 @@
 #include <SoftwareSerial.h>
 #include "midiConsts.h"
 
-#define LED_DATA_PIN 7
-#define LED_CHIP WS2811
-#define LED_COLOR_ORDER BRG
+#define LED_DATA_PIN                7
+#define LED_CHIP                    WS2811
+#define LED_COLOR_ORDER             BRG
 
-#define LED_NUM 62
+#define LED_NUM                     62
 
-#define LED_BRIGHTNESS_MAX 100
+#define LED_BRIGHTNESS_MAX          100
 
-#define LED_GROUP_INDEX_1_START  0
-#define LED_GROUP_INDEX_1_END    7
-#define LED_GROUP_INDEX_2_START  8
-#define LED_GROUP_INDEX_2_END    11
-#define LED_GROUP_INDEX_3_START  12
-#define LED_GROUP_INDEX_3_END    17
-#define LED_GROUP_INDEX_4_START  18
-#define LED_GROUP_INDEX_4_END    21
-#define LED_GROUP_INDEX_5_START  22
-#define LED_GROUP_INDEX_5_END    29
-#define LED_GROUP_INDEX_6_START  30
-#define LED_GROUP_INDEX_6_END    37
-#define LED_GROUP_INDEX_7_START  38
-#define LED_GROUP_INDEX_7_END    41
-#define LED_GROUP_INDEX_8_START  42
-#define LED_GROUP_INDEX_8_END    49
-#define LED_GROUP_INDEX_9_START  50
-#define LED_GROUP_INDEX_9_END    53
-#define LED_GROUP_INDEX_10_START 54
-#define LED_GROUP_INDEX_10_END   61
+#define LED_GROUP_INDEX_1_START     0
+#define LED_GROUP_INDEX_1_END       7
+#define LED_GROUP_INDEX_2_START     8
+#define LED_GROUP_INDEX_2_END       11
+#define LED_GROUP_INDEX_3_START     12
+#define LED_GROUP_INDEX_3_END       17
+#define LED_GROUP_INDEX_4_START     18
+#define LED_GROUP_INDEX_4_END       21
+#define LED_GROUP_INDEX_5_START     22
+#define LED_GROUP_INDEX_5_END       29
+#define LED_GROUP_INDEX_6_START     30
+#define LED_GROUP_INDEX_6_END       37
+#define LED_GROUP_INDEX_7_START     38
+#define LED_GROUP_INDEX_7_END       41
+#define LED_GROUP_INDEX_8_START     42
+#define LED_GROUP_INDEX_8_END       49
+#define LED_GROUP_INDEX_9_START     50
+#define LED_GROUP_INDEX_9_END       53
+#define LED_GROUP_INDEX_10_START    54
+#define LED_GROUP_INDEX_10_END      61
 
 
-#define STROBE_ON_FACTOR 0.5 // Factor between 0 and 1 how long the LEDs stay ON
+#define STROBE_ON_FACTOR            0.5 // Factor between 0 and 1 how long the LEDs stay ON
 
-#define BREATH_PERIOD_IN_MILLIS 5000
-#define NOISE_PERIOD_IN_MILLIS 10
-#define RAINBOW_PERIOD_IN_MILLIS 10
-#define PUMP_PERIOD_IN_MILLIS 100
+#define BREATH_PERIOD_IN_MILLIS     5000
+#define NOISE_PERIOD_IN_MILLIS      10
+#define RAINBOW_PERIOD_IN_MILLIS    10
+#define PUMP_PERIOD_IN_MILLIS       100
 
 void LEDC_init(SoftwareSerial* logSerial);
 void LEDC_updateStripe(const bool* noteOn, unsigned long triggerMillis);
