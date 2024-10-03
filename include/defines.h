475c9f9e6e2ea159c21eb0b0091e9cee21933a7b
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2024-10-03 14:50:24 +0200

    extract defines for other latters

---
+++ include/defines.h
@@ -0,0 +1,86 @@
+#ifndef RSGLED_DEFINES_H
+#define RSGLED_DEFINES_H
+
+// MAIN DEFINE FOR LETTER
+#define R
+
+#ifdef R
+
+#define LED_NUM                     62
+#define LED_GROUP_INDEX_1_START     0
+#define LED_GROUP_INDEX_1_END       7
+#define LED_GROUP_INDEX_2_START     8
+#define LED_GROUP_INDEX_2_END       11
+#define LED_GROUP_INDEX_3_START     12
+#define LED_GROUP_INDEX_3_END       17
+#define LED_GROUP_INDEX_4_START     18
+#define LED_GROUP_INDEX_4_END       21
+#define LED_GROUP_INDEX_5_START     22
+#define LED_GROUP_INDEX_5_END       29
+#define LED_GROUP_INDEX_6_START     30
+#define LED_GROUP_INDEX_6_END       37
+#define LED_GROUP_INDEX_7_START     38
+#define LED_GROUP_INDEX_7_END       41
+#define LED_GROUP_INDEX_8_START     42
+#define LED_GROUP_INDEX_8_END       49
+#define LED_GROUP_INDEX_9_START     50
+#define LED_GROUP_INDEX_9_END       53
+#define LED_GROUP_INDEX_10_START    54
+#define LED_GROUP_INDEX_10_END      61
+
+#endif
+
+#ifdef S
+
+#define LED_NUM                     62
+#define LED_GROUP_INDEX_1_START     0
+#define LED_GROUP_INDEX_1_END       7
+#define LED_GROUP_INDEX_2_START     8
+#define LED_GROUP_INDEX_2_END       11
+#define LED_GROUP_INDEX_3_START     12
+#define LED_GROUP_INDEX_3_END       17
+#define LED_GROUP_INDEX_4_START     18
+#define LED_GROUP_INDEX_4_END       21
+#define LED_GROUP_INDEX_5_START     22
+#define LED_GROUP_INDEX_5_END       29
+#define LED_GROUP_INDEX_6_START     30
+#define LED_GROUP_INDEX_6_END       37
+#define LED_GROUP_INDEX_7_START     38
+#define LED_GROUP_INDEX_7_END       41
+#define LED_GROUP_INDEX_8_START     42
+#define LED_GROUP_INDEX_8_END       49
+#define LED_GROUP_INDEX_9_START     50
+#define LED_GROUP_INDEX_9_END       53
+#define LED_GROUP_INDEX_10_START    54
+#define LED_GROUP_INDEX_10_END      61
+
+#endif
+
+#ifdef G
+
+#define LED_NUM                     62
+#define LED_GROUP_INDEX_1_START     0
+#define LED_GROUP_INDEX_1_END       7
+#define LED_GROUP_INDEX_2_START     8
+#define LED_GROUP_INDEX_2_END       11
+#define LED_GROUP_INDEX_3_START     12
+#define LED_GROUP_INDEX_3_END       17
+#define LED_GROUP_INDEX_4_START     18
+#define LED_GROUP_INDEX_4_END       21
+#define LED_GROUP_INDEX_5_START     22
+#define LED_GROUP_INDEX_5_END       29
+#define LED_GROUP_INDEX_6_START     30
+#define LED_GROUP_INDEX_6_END       37
+#define LED_GROUP_INDEX_7_START     38
+#define LED_GROUP_INDEX_7_END       41
+#define LED_GROUP_INDEX_8_START     42
+#define LED_GROUP_INDEX_8_END       49
+#define LED_GROUP_INDEX_9_START     50
+#define LED_GROUP_INDEX_9_END       53
+#define LED_GROUP_INDEX_10_START    54
+#define LED_GROUP_INDEX_10_END      61
+
+#endif
+
+
+#endif //RSGLED_DEFINES_H
