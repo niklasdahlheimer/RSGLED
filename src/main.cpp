6bb90ff768b4349ffe6d9432ae922825a0d77dcc
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2024-10-22 15:08:55 +0200

    init ble controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "midiController.h"
 #include "ledController.h"
+#include "midiControllerBle.h"
 
 MidiData *midiData;
 
@@ -7,10 +8,11 @@ void setup() {
     Serial.begin(115200);
     LEDC_init();
     MIDIC_init();
+    MIDICBLE_init();
 }
 
 void loop() {
-    midiData = MIDIC_read();
+    midiData = MIDICBLE_read();
     if(TEST_MODE){
         midiData->noteOn[RAINBOW] = true;
     }
