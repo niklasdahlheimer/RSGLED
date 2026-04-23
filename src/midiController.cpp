8a6c9b41fc4be5f437d8c3f790d7ff12e0b40192
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2026-04-23 12:53:16 +0200

    read all channels

--- src/midiController.cpp
+++ src/midiController.cpp
@@ -34,11 +34,8 @@ void MIDIC_init(const byte _midiChannel, MidiData *_midiData) {
 MidiData *MIDIC_read() {
     // while damit alles in einem step abgeholt wird
     // (wg. Problemen dass overlays zu spät triggern)
-    while (MIDI.read(MIDI_CHANNEL_ALL)) {
+    while (MIDI.read()) {
     }
-    while (MIDI.read(midiChannel)) {
-    }
-
     connectionLED.loop();
     midiLED.loop();
 
