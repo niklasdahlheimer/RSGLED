442d8bd8a044353ba310fb6322eebf9e8e1b9b64
Niklas Dahlheimer <niklasdahlheimer@web.de>
2026-05-15 17:30:47 +0200

    make overlayBreath tempo aware

--- include/overlayBreath.h
+++ include/overlayBreath.h
@@ -3,12 +3,11 @@
 
 #include <fxBase.h>
 
-#define BREATH_PERIOD_IN_MILLIS     5000
 
 /**
  * @brief Overlay: Erzeugt einen pulsierenden "Atmungs"-Effekt.
  * 
- * Die Helligkeit aller LEDs wird sinusförmig über einen Zeitraum (BREATH_PERIOD_IN_MILLIS)
+ * Die Helligkeit aller LEDs wird sinusförmig über einen Zeitraum (abhängig vom Tempo)
  * variiert, was ein langsames Aufleuchten und Abblenden bewirkt.
  */
 class OverlayBreath final : public FXBase {
@@ -22,7 +21,7 @@ protected:
     void makeEffect(LEDConfig &ledConfig, const byte velocity) override {
         const double timeFactor = pow((1 +
                                        sin(2 * M_PI * static_cast<double>(ledConfig.timestamp - startMillis) /
-                                           BREATH_PERIOD_IN_MILLIS -
+                                           getBeatLenInMillis(ledConfig.tempo, 1) -
                                            M_PI / 2)) / 2, 2); // between 0 and 1
         const double currBrightness = (velocity / 255.0) * timeFactor * LED_BRIGHTNESS_MAX;
         ledConfig.allBrighten(static_cast<byte>(currBrightness));
