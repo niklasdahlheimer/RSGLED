e53aac4b4d61a4a4daeab48df3f69a729fc7b3fa
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2026-05-26 09:16:32 +0200

    breath effect over 2 bars

--- include/overlayBreath.h
+++ include/overlayBreath.h
@@ -21,7 +21,7 @@ protected:
     void makeEffect(LEDConfig &ledConfig, const byte velocity) override {
         const double timeFactor = pow((1 +
                                        sin(2 * M_PI * static_cast<double>(ledConfig.timestamp - startMillis) /
-                                           getBeatLenInMillis(ledConfig.tempo, 1) -
+                                           getBeatLenInMillis(ledConfig.tempo, 1) / 2 -
                                            M_PI / 2)) / 2, 2); // between 0 and 1
         const double currBrightness = (velocity / 255.0) * timeFactor * LED_BRIGHTNESS_MAX;
         ledConfig.allBrighten(static_cast<byte>(currBrightness));
